9220e241bf9b9165782408cd4750cbba6370328c
trzmiel.filip <trzmiel.filip@STACJA105-07>
2024-01-03 14:15:55 +0100

    Dodaj pliki projektów.

---
+++ Egzamin/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Egzamin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            zdjecia.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Image finalImage = new Image();
+
+            BitmapImage logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri("\pocztowka.png\");
+
+            if (pocztowka.IsChecked == true)
+            {
+            }
+        }
+
+        private void pocztowka_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
