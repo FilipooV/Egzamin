61795ca958e16f015601ec8d53c2bcd0ab4db413
FilipooV <filiptrzmiel@gmail.com>
2024-01-07 01:37:37 +0100

    Add methods, add images, change apperance

--- Egzamin/MainWindow.xaml.cs
+++ Egzamin/MainWindow.xaml.cs
@@ -19,25 +19,53 @@ namespace Egzamin
         public MainWindow()
         {
             InitializeComponent();
-            zdjecia.Visibility = Visibility.Hidden;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Image finalImage = new Image();
-
-            BitmapImage logo = new BitmapImage();
-            logo.BeginInit();
-            logo.UriSource = new Uri("\pocztowka.png\");
-
-            if (pocztowka.IsChecked == true)
-            {
-            }
-        }
 
         private void pocztowka_Checked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (pocztowka.IsChecked == true)
+            {
+                cena_produktu.Content = "1 zł";
+                zdjecie.Source = new BitmapImage(new Uri("pocztowka.png", UriKind.Relative));
+
+            }
+            else if (list.IsChecked == true)
+            {
+                cena_produktu.Content = "1,5 zł";
+                zdjecie.Source = new BitmapImage(new Uri("list.png", UriKind.Relative));
+
+            }
+            else
+            {
+                cena_produktu.Content = "10zł";
+                zdjecie.Source = new BitmapImage(new Uri("paczka.png", UriKind.Relative));
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int parsedVal;
+
+            if (!int.TryParse(kod.Text, out parsedVal))
+            {
+                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
+            }
+            else if (kod.Text.Length == 5)
+            {
+                MessageBox.Show("Dane przesyłki zostały wprowadzone");
+            }
+            else if (kod.Text.Length > 5 || kod.Text.Length < 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+            }
         }
     }
 }
